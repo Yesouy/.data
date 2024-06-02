c88dd2f016d1fb6a33a66980f224e87edc337e98
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-02 20:38:39 +0900

    Update 2.30 pcTaskGetName.c

--- 2.30 pcTaskGetName.c
+++ 2.30 pcTaskGetName.c
@@ -4,7 +4,6 @@
 
 // 2.30 pcTaskGetName.c
 
-// 태스크 함수
 void vTaskGetName(void *pvParameters)
 {
     TaskHandle_t xTask = xTaskGetCurrentTaskHandle();
@@ -13,22 +12,18 @@ void vTaskGetName(void *pvParameters)
 
     for (;;)
     {
-        vTaskDelay(1000); // 1초 대기
+        vTaskDelay(1000);
     }
 }
 
 int main(void)
 {
-    // 태스크 핸들 선언
     TaskHandle_t xTaskHandle;
 
-    // vTaskGetName 태스크 생성
     xTaskCreate(vTaskGetName, "Task 1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xTaskHandle);
 
-    // FreeRTOS 스케줄러 시작
     vTaskStartScheduler();
 
-    // 스케줄러가 시작되지 않으면 이 코드는 실행됩니다.
     for (;;);
 
     return 0; // 이 줄은 실행되지 않음
