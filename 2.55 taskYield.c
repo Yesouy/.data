ed2ada05a7cb5ae5fc37a6a73ba35288d543e804
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-04 16:57:46 +0900

    Update 2.55 taskYield.c

--- 2.55 taskYield.c
+++ 2.55 taskYield.c
@@ -1,11 +1,26 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vTaskYield(void *pvParameters)
-{
-    for (;;)
-    {
-        printf("Task Yield Example\r\n");
-        taskYIELD(); // 다른 태스크에게 CPU 제어권 양도
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+
+int main(void) {
+    xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, NULL);
+    xTaskCreate(vTask2, "Task 2", 1000, NULL, 1, NULL);
+    vTaskStartScheduler();
+    for(;;);
+}
+
+void vTask1(void *pvParameters) {
+    for (;;) {
+        // 다른 작업에게 CPU 양보
+        taskYIELD();
+    }
+}
+
+void vTask2(void *pvParameters) {
+    for (;;) {
+        // 다른 작업 수행
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
