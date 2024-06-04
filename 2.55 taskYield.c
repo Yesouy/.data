80fbeedb65a5fca62eb231fe8f15b3d3773de835
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-04 21:03:23 +0900

    Update 2.55 taskYield.c

--- 2.55 taskYield.c
+++ 2.55 taskYield.c
@@ -13,14 +13,14 @@ int main(void) {
 
 void vTask1(void *pvParameters) {
     for (;;) {
-        // 다른 작업에게 CPU 양보
+
         taskYIELD();
     }
 }
 
 void vTask2(void *pvParameters) {
     for (;;) {
-        // 다른 작업 수행
+
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
