b8ba9339a0e14fd538accf7b6e14e329b46660e5
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-04 16:57:01 +0900

    Update 2.42 xTaskNotifyWait.c

--- 2.42 xTaskNotifyWait.c
+++ 2.42 xTaskNotifyWait.c
@@ -1,13 +1,22 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 
-void vTaskNotifyWait(void *pvParameters)
-{
-    uint32_t ulNotificationValue;
-    for (;;)
-    {
-        if (xTaskNotifyWait(0x00, ULONG_MAX, &ulNotificationValue, portMAX_DELAY) == pdPASS) {
-            printf("Received Notification with value: %lu\r\n", ulNotificationValue);
+TaskHandle_t xTask1Handle = NULL;
+
+void vTask1(void *pvParameters);
+
+int main(void) {
+    xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, &xTask1Handle);
+    vTaskStartScheduler();
+    for(;;);
+}
+
+void vTask1(void *pvParameters) {
+    uint32_t ulValue;
+    for (;;) {
+        if (xTaskNotifyWait(0x00, 0xFFFFFFFF, &ulValue, portMAX_DELAY) == pdTRUE) {
+            // 알림 값 처리
         }
     }
 }
