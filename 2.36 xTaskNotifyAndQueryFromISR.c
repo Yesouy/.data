e0da23025b09dbbb9f33def960638bd3fc0f017c
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-04 16:56:25 +0900

    Update 2.36 xTaskNotifyAndQueryFromISR.c

--- 2.36 xTaskNotifyAndQueryFromISR.c
+++ 2.36 xTaskNotifyAndQueryFromISR.c
@@ -1,10 +1,32 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 
-void vTaskNotify(void *pvParameters)
-{
-    const TickType_t xMaxBlockTime = pdMS_TO_TICKS(200);
-    uint32_t ulValueToSend = 10;
-    printf("Sending notification with value %lu\r\n", ulValueToSend);
-    xTaskNotifyGiveIndexed(xTaskHandle, 0);
+TaskHandle_t xTask1Handle = NULL;
+
+void vTask1(void *pvParameters);
+void ISR_Handler(void);
+
+int main(void) {
+    xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, &xTask1Handle);
+    vTaskStartScheduler();
+    for(;;);
+}
+
+void vTask1(void *pvParameters) {
+    uint32_t ulValue;
+    for (;;) {
+        if (xTaskNotifyWait(0x00, 0xFFFFFFFF, &ulValue, portMAX_DELAY) == pdTRUE) {
+            // 알림 값 처리
+        }
+    }
+}
+
+void ISR_Handler(void) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint32_t ulPreviousNotifyValue;
+
+    xTaskNotifyAndQueryFromISR(xTask1Handle, 0x01, eSetValueWithOverwrite, &ulPreviousNotifyValue, &xHigherPriorityTaskWoken);
+
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
