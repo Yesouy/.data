1483458c2e5ed14a167883ba37aefc9fffd7f2d6
Yesouy._ <106993133+Yesouy@users.noreply.github.com>
2024-06-04 16:57:18 +0900

    Update 2.48 xTaskSetApplicationTaskTag.c

--- 2.48 xTaskSetApplicationTaskTag.c
+++ 2.48 xTaskSetApplicationTaskTag.c
@@ -1,9 +1,24 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vTaskSetApplicationTaskTag(void *pvParameters)
-{
-    TaskHandle_t xTask = xTaskGetCurrentTaskHandle();
-    vTaskSetApplicationTaskTag(xTask, (TaskHookFunction_t)1);
-    printf("Task Tag set to 1\r\n");
+TaskHandle_t xTask1Handle = NULL;
+
+void vTask1(void *pvParameters);
+
+int main(void) {
+    xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, &xTask1Handle);
+
+    // 태그 설정
+    vTaskSetApplicationTaskTag(xTask1Handle, (void *)1);
+
+    vTaskStartScheduler();
+    for(;;);
+}
+
+void vTask1(void *pvParameters) {
+    for (;;) {
+        // 태그 값 읽기
+        void *tag = pvTaskGetApplicationTaskTag(NULL);
+        // 태그 값 처리
+    }
 }
